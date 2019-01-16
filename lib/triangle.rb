736bddf646fb5ef209f5ad7780d91c0ea6ec7260
Sara Walker <sarawall1187@gmail.com>
2019-01-16 19:21:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,18 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :side1, :side2, :side3, :equilateral, :isosceles, :scalene
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  def kind(side1, side2, side3) 
+    @equilateral = side1 == side2 == side3
+    @isosceles = 
+    @scalene = 
+    self.kind
+  end
+    
 end
